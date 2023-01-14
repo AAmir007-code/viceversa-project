42494bd15850e3c8d6a28b481f83b82242ef929c
AAmir007-code <amiraslonov00@gmail.com>
2023-01-14 05:32:55 -0500

    Adding Bootstrap

--- viceversa/urls.py
+++ viceversa/urls.py
@@ -18,6 +18,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home),
+    path('',views.home, name='homepage'),
     path('reversed/',views.reverse, name='reverse'),
 ]
